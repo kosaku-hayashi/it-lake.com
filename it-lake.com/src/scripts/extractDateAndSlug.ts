e9321afcd51678c84b686a1d384e8cd92e7dc263
kosaku-hayashi <hayasi.mkmk128@gmail.com>
2024-11-11 18:17:09 +0900

    Changed folder structure of content collection

--- it-lake.com/src/scripts/extractDateAndSlug.ts
+++ it-lake.com/src/scripts/extractDateAndSlug.ts
@@ -1,14 +1,17 @@
+import { extractFileName } from './extractFileName';
+
 interface ExtractedData {
   pubDate: string;
   slug: string;
 }
 
-export function extractDateAndSlug(filename: string): ExtractedData {
+export function extractDateAndSlug(path: string): ExtractedData {
+  const fileName = extractFileName(path, true);
   const pattern = /^(\d{4}-\d{2}-\d{2})-([a-z0-9-]+)\.mdx$/;
-  const match = filename.match(pattern);
+  const match = fileName.match(pattern);
   if (!match) {
     throw new Error(
-      `Invalid filename format. Expected "{yyyy-mm-dd}-{slug}.mdx", received "${filename}"`
+      `Invalid filename format. Expected "{yyyy-mm-dd}-{slug}.mdx", received "${fileName}"`
     );
   }
 
--- it-lake.com/src/scripts/extractFileName.ts
+++ it-lake.com/src/scripts/extractFileName.ts
@@ -1,11 +1,11 @@
-export function extractFileName(src: string | ImageMetadata): string {
+export function extractFileName(src: string | ImageMetadata, includeExtension: boolean = false): string {
   src = typeof src === "string" ? src : (src as ImageMetadata).src;
   const cleanPath = src.split('?')[0].split('#')[0];
   const segments = cleanPath.split('/');
   const lastSegment = segments.pop() || '';
 
   const dotIndex = lastSegment.lastIndexOf('.');
-  if (dotIndex === -1) return lastSegment;
+  if (dotIndex === -1 || includeExtension) return lastSegment;
 
   return lastSegment.substring(0, dotIndex);
 }
