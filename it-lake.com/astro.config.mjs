6cffa7434049f3ac9513a5292a8a6b86fcaa5429
kosaku-hayashi <hayasi.mkmk128@gmail.com>
2024-11-15 17:34:08 +0900

    update README.md add analytics

--- it-lake.com/astro.config.mjs
+++ it-lake.com/astro.config.mjs
@@ -2,15 +2,23 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: "https://it-lake.com",
-  integrations: [mdx(),sitemap({
-    filter: (page) =>
-      page !== 'https://it-lake.com/posts/csharptest/' &&
-      page !== 'https://it-lake.com/posts/mdx-test/',
-      // lastmod: new Date('2022-02-24'),
-  })],
+  integrations: [
+    mdx(),
+    partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    }}),
+    sitemap({
+      filter: (page) =>
+        page !== 'https://it-lake.com/posts/csharptest/' &&
+        page !== 'https://it-lake.com/posts/mdx-test/',
+        // lastmod: new Date('2022-02-24'),
+      })
+  ],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
@@ -18,4 +26,7 @@ export default defineConfig({
       wrap:false, // 水平スクロールを防止するために行末で折り返す
     },
   },
+  redirects: {
+    '/privacy-policy': 'https://lite-notion.com/privacy-policy'
+  }
 });
